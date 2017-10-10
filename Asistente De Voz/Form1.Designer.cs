57d04cf0db37ea5a6e18e7fdcccf4e02fa7218b0
crist <crist@CRISTIANPINEDA1>
2017-10-10 14:58:04 -0600

    Agregar archivos de proyecto.

---
+++ Asistente De Voz/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Asistente_De_Voz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.Activar = new System.Windows.Forms.Button();
+            this.Detener = new System.Windows.Forms.Button();
+            this.Estado = new System.Windows.Forms.Label();
+            this.resultado = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            resources.ApplyResources(this.linkLabel1, "linkLabel1");
+            this.linkLabel1.BackColor = System.Drawing.Color.Black;
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Activar
+            // 
+            this.Activar.BackColor = System.Drawing.Color.Green;
+            this.Activar.BackgroundImage = global::Asistente_De_Voz.Properties.Resources.microfono_grabador1;
+            resources.ApplyResources(this.Activar, "Activar");
+            this.Activar.Name = "Activar";
+            this.Activar.UseVisualStyleBackColor = false;
+            this.Activar.Click += new System.EventHandler(this.Activar_Click);
+            // 
+            // Detener
+            // 
+            this.Detener.BackColor = System.Drawing.Color.Red;
+            this.Detener.BackgroundImage = global::Asistente_De_Voz.Properties.Resources.mutevoice;
+            resources.ApplyResources(this.Detener, "Detener");
+            this.Detener.Name = "Detener";
+            this.Detener.UseVisualStyleBackColor = false;
+            this.Detener.Click += new System.EventHandler(this.Detener_Click);
+            // 
+            // Estado
+            // 
+            resources.ApplyResources(this.Estado, "Estado");
+            this.Estado.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Estado.ForeColor = System.Drawing.Color.Snow;
+            this.Estado.Name = "Estado";
+            // 
+            // resultado
+            // 
+            resources.ApplyResources(this.resultado, "resultado");
+            this.resultado.BackColor = System.Drawing.Color.Snow;
+            this.resultado.Name = "resultado";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Name = "label3";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::Asistente_De_Voz.Properties.Resources._18926346_1309854035796351_723634850_o;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.Estado);
+            this.Controls.Add(this.Detener);
+            this.Controls.Add(this.Activar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button Activar;
+        private System.Windows.Forms.Button Detener;
+        private System.Windows.Forms.Label Estado;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Asistente De Voz/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+using WMPLib;
+
+namespace Asistente_De_Voz
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer sonido = new WindowsMediaPlayer();
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        bool HabilitarReconocimiento = true;
+        string opc;
+        public Form1()
+        {
+            InitializeComponent();
+            Activar.BackColor = Color.Green;
+            Detener.Visible = false;
+            label3.Visible = false;
+            resultado.Text = "OFF";
+            resultado.BackColor = Color.Red;
+            sonido.URL = @"Media\Audio\hola.wav";
+            sonido.controls.play();
+        }
+
+        private void Activar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (resultado.Text == "OFF")
+                {
+                    sonido.URL = @"Media\Audio\hola2.wav";
+                    sonido.controls.play();
+                }
+                else if (resultado.Text == "ON")
+                {
+                    sonido.URL = @"Media\Audio\hola2.wav";
+                    sonido.controls.stop();
+                }
+                resultado.Text = "ON";
+                resultado.BackColor = Color.Green;
+                Detener.Visible = true;
+                label3.Visible = true;
+                Activar.Visible = false;
+                label2.Visible = false;
+                Detener.BackColor = Color.Red;
+                _recognizer.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"Media\comandos.txt")))));
+                _recognizer.SpeechRecognized += _recognizer_SpeechRecognized;
+                _recognizer.RecognizeCompleted += _recognizer_RecognizeCompleted;
+                _recognizer.RecognizerUpdateReached += _recognizer_RecognizerUpdateReached;
+                _recognizer.SetInputToDefaultAudioDevice();
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            }
+            catch (Exception error)
+            {
+                sonido.controls.stop();
+                Activar.BackColor = Color.Red;
+                Detener.BackColor = Color.Green;
+                resultado.Text = "OFF";
+                resultado.BackColor = Color.Red;
+                MessageBox.Show(error.Message, "Se Ha Encontrado Un Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+
+
+        }
+
+        private void _recognizer_RecognizerUpdateReached(object sender, RecognizerUpdateReachedEventArgs e)
+        {
+            HabilitarReconocimiento = false;
+        }
+
+        private void _recognizer_RecognizeCompleted(object sender, RecognizeCompletedEventArgs e)
+        {
+           HabilitarReconocimiento = true;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Detener_Click(object sender, EventArgs e)
+        {
+            if (Activar.BackColor != Color.Green)
+            {
+                MessageBox.Show("El Asistente Esta Desactivado", "Active El Asistente", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                resultado.Text = "OFF";
+                resultado.BackColor = Color.Red;
+                Detener.Visible = false;
+                label3.Visible = false;
+                Activar.Visible = true;
+                label2.Visible = true;
+                Detener.BackColor = Color.Red;
+                sonido.URL = @"Media\Audio\detener.wav";
+                sonido.controls.play();
+                _recognizer.RecognizeAsyncStop();
+            }
+        }
+        public void _recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            opc = e.Result.Text;
+            if (HabilitarReconocimiento == true)
+            {
+                switch (opc)
+                {
+                    //-----------------------------esto ya es para los comandos Todos Con If------------------------------------------//
+
+                    case "oyedeltaabregoogle....":             
+                        sonido.URL = @"Media\Audio\google.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("chrome.exe");
+                        break;
+
+                    case "oyedeltaabreexcel....":
+                        sonido.URL = @"Media\Audio\excel.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("excel.exe");
+                        break;
+
+                    case "oyedeltaabreword....":
+                        sonido.URL = @"Media\Audio\word.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("winword.exe");
+                        break;
+
+                    case "adiosdelta....":
+                        sonido.URL = @"Media\Audio\adiosdelta.wav";
+                        sonido.controls.play();
+                        Application.Exit();
+                        break;
+
+                    case "oyedeltaabreblocdenotas....":
+                        sonido.URL = @"Media\Audio\notepad.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("notepad.exe");
+                        break;
+
+                    case "oyedeltaabreyoutube....":
+                        sonido.URL = @"Media\Audio\youtube.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("http://www.youtube.com");
+                        break;
+
+                    case "oyedeltaabrefacebook....":
+                        sonido.URL = @"Media\Audio\facebook.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("http://www.facebook.com");
+                        break;
+
+                    case "oyedeltaabremusica....":
+                        sonido.URL = @"Media\Audio\wmp.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("wmplayer.exe");
+                        break;
+
+                    case "DeltaQueesAndroid....":
+                        sonido.URL = @"Media\Audio\android.wav";
+                        sonido.controls.play();
+                        break;
+
+                    case "oyedeltaabrewhatsappweb....":
+                        sonido.URL = @"Media\Audio\whatsapp.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("https://web.whatsapp.com");
+                        break;
+
+                    case "holadelta....":
+                        sonido.URL = @"Media\Audio\holadelta.wav";
+                        sonido.controls.play();
+                        break;
+
+                    case "comoestas....":
+                        sonido.URL = @"Media\Audio\gracias.wav";
+                        sonido.controls.play();
+                        break;
+
+                    case "muybien....":
+                        sonido.URL = @"Media\Audio\bien.wav";
+                        sonido.controls.play();
+                        break;
+                    case "puesmuybien....":
+                        sonido.URL = @"Media\Audio\bien.wav";
+                        sonido.controls.play();
+                        break;
+
+                    case "oyedeltaabrepaneldecontrol....":
+                        sonido.URL = @"Media\Audio\google.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("control");
+                        break;
+
+                    case "oyedeltaabrepropiedadesdefechayhora....":
+                        sonido.URL = @"Media\Audio\google.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("timedate.cpl");
+                        break;
+
+                    case "oyedeltaabrepropiedadesdepantalla....":
+                        sonido.URL = @"Media\Audio\google.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("desk.cpl");
+                        break;
+
+                    case "oyedeltaabrecalculadora....":
+                        sonido.URL = @"Media\Audio\google.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("calc");
+                        break;
+
+                    case "oyedeltaabrepaint....":
+                        sonido.URL = @"Media\Audio\google.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("mspaint");
+                        break;
+
+                    case "oyedeltaapagarequipo....":
+                        sonido.URL = @"Media\Audio\google.wav";
+                        sonido.controls.play();
+                        System.Diagnostics.Process.Start("shutdown");
+                        break;
+
+                    //--------------------------------------Fin De La Lineas De Comandos---------------------------------------------//
+                }
+            }
+            else
+            {
+                HabilitarReconocimiento = false;
+            }
+        }
+    }
+}
+
+
